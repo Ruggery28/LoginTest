46e24e6ba305befc647dcfaf9be556d3345a6035
Ruggery <Ruggery@Ruggery>
2025-01-13 22:05:34 +0000

    Second Commit > Learning about ArrayList and Loop(For).

--- src/tasklist/TaskList.java
+++ src/tasklist/TaskList.java
@@ -4,6 +4,7 @@
  */
 package tasklist;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -19,25 +20,75 @@ public class TaskList {
 
         Scanner kBoard = new Scanner(System.in);
         String email, password;
+
+//        System.out.printf("Enter your email: ");
+//        email = kBoard.nextLine();
+//        System.out.printf("Enter your password: ");
+//        password = kBoard.nextLine();
+//        
+//        /*Object user was created so it can be used along the class User
+//        User user = new User();
+//        I'm sending email and password to the class User and storing it in the 
+//        private variables using the method Get*/
+
+//        user.setEmail(email);
+//        user.setPassword(password);
+//        
+//        /*Now, I'm getting the information that was stored in the variables in
+//        the User class and printing on the screen.*/
+
+//        System.out.println("email: " + user.getEmail());
+//        System.out.println("password: " + user.getPassword());
         
-        System.out.println("Enter your email: ");
-        email = kBoard.nextLine();
-        System.out.println("Enter your password: ");
-        password = kBoard.nextLine();
+            //NOW LEARNING ARRYS AND LOOP
+        ArrayList<User> users = new ArrayList();
         
-        User user = new User();
-        user.getEmail(email);
-        user.getPassword(password);
+        for (int i = 0; i < 3; i++) {
+            System.out.printf("Enter your email: ");
+            email = kBoard.nextLine();
+            System.out.printf("Enter your password: ");
+            password = kBoard.nextLine();
+            
+            /*
+            Object user was created so it can be used along the class User with
+            the methods Set.
+            */
+            
+            User user = new User();
+            user.setEmail(email);
+            user.setPassword(password);
+            
+            /*
+            The user will add the email and passoword and it will be stored inside
+            the arraylist that was created outside the for.
+            */
+            
+            users.add(user);
+        }
         
-        System.out.println("email: " + user.setEmail());
-        System.out.println("password: " + user.setPassword());
         
-        
-        
-        
-        
+        for (int i = 0; i < users.size(); i++){
+            
+            /*The variable userTemp will be a temporary variable that will store
+            and get the value inside the Array, inside the brakets I'm using (i)
+            that means they will get the first value and when the loops start again
+            it will get the second and so on, because I'm incringing it in the FOR*/
+            
+            User userTemp = users.get(i);
+            
+            /*
+            Now, printing it on screen, and we can do it in two different ways,
+            first one using the variable userTemp(will point the first value in the 
+            Array) + getEmail(will get the first value that was stored), Second way
+            is to use the Array method Get and then the User method in sequence.
+            */
+            
+            System.out.println("email" + i + " is: "+ userTemp.getEmail()); //users.get(i).getEmail()
+            System.out.println("password" + i + " is: " + userTemp.getPassword()); //users.get(i).getPassword()
+            
+        }
         
         
     }
-    
+
 }
